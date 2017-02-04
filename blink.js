912e2400a7a88452309a5c861e1d6d431ca350e7
Ryan Gill <ryanjgill2@gmail.com>
2017-02-03 23:48:11 -0600

    trying pin A0

--- blink.js
+++ blink.js
@@ -1,7 +1,7 @@
 var m = require('mraa');
 var ledState = true;
 var myLed = new m.Gpio(44);
-var temp = new m.Aio(18);
+var temp = new m.Aio(0);
 
 myLed.dir(m.DIR_OUT);
 
@@ -9,7 +9,7 @@ myLed.dir(m.DIR_OUT);
 function periodicActivity() {
   myLed.write(ledState ? 1 : 0);
   ledState = !ledState;
-  
+
   console.log('temp: ' + temp.read());
   setTimeout(periodicActivity, 500);
 }
