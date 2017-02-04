755ef182d3b560219b39aca85af53779f3700665
Ryan Gill <ryanjgill2@gmail.com>
2017-02-04 11:29:40 -0600

    trying firmata example

---
+++ firmataBlink.js
@@ -0,0 +1,35 @@
+console.log('WWW blink start ...');
+
+var ledPin = 13;
+var firmata = require('firmata');
+
+var board = new firmata.Board("/dev/ttyS0",function(err) {
+  if (err) {
+    console.log(err);
+    board.reset();
+    return;
+  }
+
+  console.log('connected...');
+  console.log('board.firmware: ', board.firmware);
+
+  board.pinMode(ledPin, board.MODES.OUTPUT);
+
+  var url = require('url');
+  var http = require('http');
+
+  http.createServer(function(request, response) {
+    var params = url.parse(request.url, true).query;
+
+    if (params.value.toLowerCase() == 'high') {
+      board.digitalWrite(ledPin, board.HIGH);
+    } else {
+      board.digitalWrite(ledPin, board.LOW);
+    }
+    response.writeHead(200);
+    response.write("The value written was: " + params.value);
+    response.end();
+  }.bind(this)).listen(3030);
+
+  console.log('Listening on port 3030 ...');
+});
