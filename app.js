9e37968ed6a172224cc088e4579e906f3421b741
Ryan Gill <ryanjgill2@gmail.com>
2017-02-03 21:49:14 -0600

    making changes for smaller footprint

--- app.js
+++ app.js
@@ -1,51 +1,49 @@
-const express = require('express')
-	, app = express()
-	, http = require('http').Server(app)
-	, routeLoader = require('express_utils')
-	, ROUTERPREFIX = '/'
-	, PORT = 3000
-	, morgan = require('morgan')
-	, socketIO = require('socket.io')(http)
-	;
+const router = require('tiny-router')
+  , fs = require('fs')
+  , http = require('http').createServer(router.Router())
+  , PORT = 3000
+  , socketIO = require('socket.io')(http)
+  ;
 
-// setting app constants
-app.locals.__title = 'Chameleon Monitor';
-app.locals.__description = 'Monitor UV light, temperature, and misting system water level.'
+router.use('defaultPage', './public/views/index.html');
 
-
-app.use(morgan('dev'));
-
-app.use('/public', express.static(__dirname + '/public'));
-
-// using pug templates
-app.set('view engine', 'pug');
+router.use('public', {path: __dirname + '/public'});
 
 // render index page
-app.get('/', function (req, res) {
-  res.render('index');
+router.get('/', function (req, res) {
+  fs.readFile('./public/views/index.html', function (err, html) {
+    if (err) {
+      throw err;
+    }
+    res.writeHeader(200, {"Content-Type": "text/html"});
+    res.write(html);
+    res.end();
+  });
 });
-
 
 // socket events
 socketIO.on('connection', socket => {
-	console.log('New connection!');
+  console.log('New connection!');
 
-	socket.on('newUser', data => console.log(data));
+  socket.on('newUser', data => console.log(data));
 
-	socket.on('disconnect', (text) => console.log(text));
+  socket.on('disconnect', text => console.log(text));
 });
 
 function getRandomInt(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function emitRandomValues () {
-	if (!socketIO) {return;}
 
-	let uv = getRandomInt(4, 6)/10;
-	let temp = getRandomInt(70, 90);
-	let date = Date.now();
+function emitRandomValues() {
+  if (!socketIO) {
+    return;
+  }
 
-	socketIO.emit('new-reading', {uv, temp, date});
+  let uv = getRandomInt(4, 6) / 10;
+  let temp = getRandomInt(70, 90);
+  let date = Date.now();
+
+  socketIO.emit('new-reading', {uv, temp, date});
 };
 
 setInterval(emitRandomValues, 1000);
