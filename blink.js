33e343717f257811779283f0dab1c9880fd8137b
Ryan Gill <ryanjgill2@gmail.com>
2017-02-03 23:41:35 -0600

    trying read from analog pin

--- blink.js
+++ blink.js
@@ -1,12 +1,16 @@
 var m = require('mraa');
 var ledState = true;
 var myLed = new m.Gpio(44);
+var temp = new m.Aio(18);
 
 myLed.dir(m.DIR_OUT);
+
 
 function periodicActivity() {
   myLed.write(ledState ? 1 : 0);
   ledState = !ledState;
+  
+  console.log('temp: ' + temp.read());
   setTimeout(periodicActivity, 500);
 }
 periodicActivity();
