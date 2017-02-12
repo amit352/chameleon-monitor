277dea2bc0e13902a233bb3368b88ff6af1269cf
Ryan Gill <ryanjgill2@gmail.com>
2017-02-11 19:10:59 -0600

    trying firmata

--- app.js
+++ app.js
@@ -3,54 +3,70 @@ const router = require('tiny-router')
   , http = require('http').createServer(router.Router())
   , PORT = 3030
   , socketIO = require('socket.io')(http)
+  , firmata = require('firmata')
+  , ledPin = 13
   ;
 
-
-router.use('defaultPage', './public/views/index.html');
-
-router.use('public', {path: __dirname + '/public'});
-
-// render index page
-router.get('/', function (req, res) {
-  fs.readFile('./public/views/index.html', function (err, html) {
-    if (err) {
-      throw err;
-    }
-    res.writeHeader(200, {"Content-Type": "text/html"});
-    res.write(html);
-    res.end();
-  });
-});
-
-// socket events
-socketIO.on('connection', socket => {
-  console.log('New connection!');
-
-  socket.on('newUser', data => console.log(data));
-
-  socket.on('disconnect', text => console.log(text));
-});
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function emitRandomValues() {
-  if (!socketIO) {
+const board = new firmata.Board("/dev/ttyS0", function (err) {
+  if (err) {
+    console.log(err);
+    board.reset();
     return;
   }
 
-  let uv = getRandomInt(4, 6) / 10;
-  let temp = getRandomInt(70, 90);
-  let date = Date.now();
+  console.log('connected...');
+  console.log('board.firmware: ', board.firmware);
 
-  socketIO.emit('new-reading', {uv, temp, date});
-};
+  board.pinMode(ledPin, board.MODES.OUTPUT);
+  board.digitalWrite(ledPin, 0);
 
-setInterval(emitRandomValues, 1000);
+  router.use('defaultPage', './public/views/index.html');
 
-// set the app to listen on port 3000
-http.listen(PORT);
+  router.use('public', {path: __dirname + '/public'});
 
-// log the port
-console.log(`Up and running on mylinkit.local:${PORT}`);
+  // render index page
+  router.get('/', function (req, res) {
+    fs.readFile('./public/views/index.html', function (err, html) {
+      if (err) {
+        throw err;
+      }
+      res.writeHeader(200, {"Content-Type": "text/html"});
+      res.write(html);
+      res.end();
+    });
+  });
+
+  // socket events
+  socketIO.on('connection', socket => {
+    console.log('New connection!');
+
+    socket.on('newUser', data => console.log(data));
+
+    socket.on('disconnect', text => console.log(text));
+  });
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function emitRandomValues() {
+    if (!socketIO) {
+      return;
+    }
+
+    let uv = getRandomInt(4, 6) / 10;
+    let temp = getRandomInt(70, 90);
+    let date = Date.now();
+
+    socketIO.emit('new-reading', {uv, temp, date});
+  };
+
+  setInterval(emitRandomValues, 1000);
+
+  // set the app to listen on port 3000
+  http.listen(PORT);
+
+  // log the port
+  console.log(`Up and running on mylinkit.local:${PORT}`);
+});
+
