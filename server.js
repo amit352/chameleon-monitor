33c53f99464ade1ba61f3ae2c94ba6024ebe3801
Ryan Gill <ryanjgill2@gmail.com>
2017-02-24 15:18:58 -0600

    test server, test temp

--- server.js
+++ server.js
@@ -1,44 +1,58 @@
 'use strict';
 
-var router = require('tiny-router'),
-  fs = require('fs'),
-  http = require('http').createServer(router.Router()),
-  PORT = 3030,
-  socketIO = require('socket.io')(http);
+/**
+ * This is used to run without microcontroller to test web client UI.
+ * It simulates data streaming to the client.
+ */
 
-router.use('defaultPage', './public/views/index.html');
+var express = require('express')
+  , app = express()
+  , fs = require('fs')
+  , path = require('path')
+  , http = require('http').createServer(app)
+  , socketIO = require('socket.io')(http)
+  , os = require('os')
+  , address = 'localhost'
+  , PORT = 3030
+  , ledState = 0
+  , _users = 0
+  ;
 
-router.use('public', {path: __dirname + '/public'});
+app.use('/public', express.static(__dirname + '/public'));
 
-// render index page
-router.get('/', function (req, res) {
-  fs.readFile('./public/views/index.html', function (err, html) {
-    if (err) {
-      throw err;
-    }
-    res.writeHeader(200, {"Content-Type": "text/html"});
-    res.write(html);
-    res.end();
-  });
+app.get('/', function (req, res, next) {
+  res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
 
 // socket events
 socketIO.on('connection', function (socket) {
   console.log('New connection!');
 
-  socket.on('newUser', function (data) {
-    return console.log(data);
+  socket.on('newUser', function () {
+    // update users count
+    _users = socketIO.engine.clientsCount;
+    console.log('Total users: ' + _users);
   });
 
-  socket.on('disconnect', function (text) {
-    return console.log(text);
+  socket.on('toggleLed', function () {
+    ledState = Math.abs(ledState - 1);
+    // emit led was toggled
+    socketIO.sockets.emit('led:toggled', {ledState: ledState});
+  });
+
+  socket.on('disconnect', function () {
+    // update users count
+    _users = socketIO.engine.clientsCount;
+    console.log('Total users: ' + _users);
   });
 });
 
+// used for generating mock data
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// simulates micro controller sensor data
 function emitRandomValues() {
   if (!socketIO) {
     return;
@@ -49,12 +63,13 @@ function emitRandomValues() {
   var date = Date.now();
 
   socketIO.emit('new-reading', {uv: uv, temp: temp, date: date});
-};
+}
 
+// emit random values every second
 setInterval(emitRandomValues, 1000);
 
-// set the app to listen on port 3000
+// set the app to listen on port 3030
 http.listen(PORT);
 
 // log the port
-console.log('Up and running on mylinkit.local:' + PORT);
+console.log('Up and running on ' + address + ':' + PORT);
